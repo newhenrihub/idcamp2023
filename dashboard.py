c892e1db58ff261f1e96020775953b1076c0e62b
newhenrihub <152056086+newhenrihub@users.noreply.github.com>
2023-11-26 21:49:18 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -29,7 +29,7 @@ def create_category_count_df(orders_df,order_items_df,products_df):
     
     return category_count_df
 
-def create_state_count_df(orders_df,customers_df):
+def create_order_customer_df(orders_df,customers_df):
     # merge untuk mendapatkan customer dalam data pemesanan
     order_customer_df = pd.merge(
         left=orders_df,
@@ -38,13 +38,24 @@ def create_state_count_df(orders_df,customers_df):
         left_on="customer_id",
         right_on="customer_id"
     )
+    
+    return order_customer_df
 
+def create_state_count_df(order_customer_df):
     # ambil top 5 state
     state_count_df = order_customer_df.groupby(["customer_state"])["customer_state"].count().reset_index(name="count").sort_values("count",ascending=False)
     state_count_df.rename(columns={"count":"state_count"},inplace=True)
     state_count_df.head(5)
 
     return state_count_df
+
+def create_loyal_cust_count_df(order_customer_df):
+    # ambil top 10 loyal customer
+    loyal_cust_count_df = order_customer_df.groupby(["customer_unique_id"])["customer_unique_id"].count().reset_index(name="count").sort_values("count",ascending=False)
+    loyal_cust_count_df.rename(columns={"count":"loyal_cust_count"},inplace=True)
+    loyal_cust_count_df.head(10)
+
+    return loyal_cust_count_df
 
 customers_df = pd.read_csv("https://raw.githubusercontent.com/newhenrihub/idcamp2023/main/submission/data/customers_dataset.csv", delimiter=",")
 orders_df = pd.read_csv("https://raw.githubusercontent.com/newhenrihub/idcamp2023/main/submission/data/orders_dataset.csv", delimiter=",")
@@ -54,7 +65,9 @@ products_df = pd.read_csv("https://raw.githubusercontent.com/newhenrihub/idcamp2
 products_df.dropna(axis=0, inplace=True)
 
 category_count_df = create_category_count_df(orders_df,order_items_df,products_df)
-state_count_df = create_state_count_df(orders_df,customers_df)
+order_customer_df = create_order_customer_df(orders_df,customers_df)
+state_count_df = create_state_count_df(order_customer_df)
+loyal_cust_count_df = create_loyal_cust_count_df(order_customer_df)
 
 st.header("E-Commerce Dashboard")
 
@@ -64,7 +77,7 @@ colors = ["#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 with col1:
     fig, ax = plt.subplots(figsize=(20, 20))
     sns.barplot(y="product_category_name", x="category_count", data=category_count_df.head(5), orient="h", palette=colors, ax=ax)
-    ax.set_title("Top 5 product by category", loc="center", fontsize=50)
+    ax.set_title("Top 5 orders by product category", loc="center", fontsize=50)
     ax.set_ylabel(None)
     ax.set_xlabel(None)
     ax.tick_params(axis='x', labelsize=30)
@@ -74,9 +87,19 @@ with col1:
 with col2:
     fig, ax = plt.subplots(figsize=(20, 20))
     sns.barplot(y="state_count", x="customer_state", data=state_count_df.head(5), palette=colors, ax=ax)
-    ax.set_title("Top 5 most order by state", loc="center", fontsize=50)
+    ax.set_title("Top 5 orders by state", loc="center", fontsize=50)
     ax.set_ylabel(None)
     ax.set_xlabel(None)
     ax.tick_params(axis='x', labelsize=30)
     ax.tick_params(axis='y', labelsize=30)
     st.pyplot(fig)
+
+fig, ax = plt.subplots(figsize=(20, 20))
+colors = ["#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+sns.barplot(y="customer_unique_id", x="loyal_cust_count", data=loyal_cust_count_df.head(10), palette=colors, ax=ax)
+ax.set_title("Top 10 loyal customers", loc="center", fontsize=50)
+ax.set_ylabel(None)
+ax.set_xlabel(None)
+ax.tick_params(axis='x', labelsize=30)
+ax.tick_params(axis='y', labelsize=30)
+st.pyplot(fig)

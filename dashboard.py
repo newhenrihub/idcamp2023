d4b0ff051e9571432e046e5e86a60991f533f1a1
newhenrihub <152056086+newhenrihub@users.noreply.github.com>
2023-11-26 21:20:12 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,49 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def create_category_count_df(orders_df,order_items_df,products_df):
+    # merge untuk mendapatkan item product yang ada di dalam order
+    order_detail_df = pd.merge(
+        left=orders_df,
+        right=order_items_df,
+        how="left",
+        left_on="order_id",
+        right_on="order_id"
+    )
+    
+    # merge untuk mendapatkan category product dari item yang dipesan
+    order_detail_product_df = pd.merge(
+        left=order_detail_df,
+        right=products_df,
+        how="left",
+        left_on="product_id",
+        right_on="product_id"
+    )
+    
+    # ambil top 5 category
+    category_count_df = order_detail_product_df.groupby(["product_category_name"])["product_category_name"].count().reset_index(name="count").sort_values("count",ascending=False)
+    category_count_df.rename(columns={"count":"category_count"},inplace=True)
+    category_count_df.head(5)
+    
+    return category_count_df
+
+def create_order_customer_df(orders_df,customers_df):
+    # merge untuk mendapatkan customer dalam data pemesanan
+    order_customer_df = pd.merge(
+        left=orders_df,
+        right=customers_df,
+        how="left",
+        left_on="customer_id",
+        right_on="customer_id"
+    )
+
+    # ambil top 5 state
+    state_count_df = order_customer_df.groupby(["customer_state"])["customer_state"].count().reset_index(name="count").sort_values("count",ascending=False)
+    state_count_df.rename(columns={"count":"state_count"},inplace=True)
+    state_count_df.head(5)
+
+    return order_customer_df
+
 customers_df = pd.read_csv("https://raw.githubusercontent.com/newhenrihub/idcamp2023/main/submission/data/customers_dataset.csv", delimiter=",")
 orders_df = pd.read_csv("https://raw.githubusercontent.com/newhenrihub/idcamp2023/main/submission/data/orders_dataset.csv", delimiter=",")
 order_items_df = pd.read_csv("https://raw.githubusercontent.com/newhenrihub/idcamp2023/main/submission/data/order_items_dataset.csv", delimiter=",")
@@ -10,42 +53,8 @@ products_df = pd.read_csv("https://raw.githubusercontent.com/newhenrihub/idcamp2
 
 products_df.dropna(axis=0, inplace=True)
 
-# merge untuk mendapatkan item product yang ada di dalam order
-order_detail_df = pd.merge(
-    left=orders_df,
-    right=order_items_df,
-    how="left",
-    left_on="order_id",
-    right_on="order_id"
-)
-
-# merge untuk mendapatkan category product dari item yang dipesan
-order_detail_product_df = pd.merge(
-    left=order_detail_df,
-    right=products_df,
-    how="left",
-    left_on="product_id",
-    right_on="product_id"
-)
-
-# ambil top 5 category
-category_count_df = order_detail_product_df.groupby(["product_category_name"])["product_category_name"].count().reset_index(name="count").sort_values("count",ascending=False)
-category_count_df.rename(columns={"count":"category_count"},inplace=True)
-category_count_df.head(5)
-
-# merge untuk mendapatkan customer dalam data pemesanan
-order_customer_df = pd.merge(
-    left=orders_df,
-    right=customers_df,
-    how="left",
-    left_on="customer_id",
-    right_on="customer_id"
-)
-
-# ambil top 5 state
-state_count_df = order_customer_df.groupby(["customer_state"])["customer_state"].count().reset_index(name="count").sort_values("count",ascending=False)
-state_count_df.rename(columns={"count":"state_count"},inplace=True)
-state_count_df.head(5)
+category_count_df = create_category_count_df(orders_df,order_items_df,products_df)
+order_customer_df = create_order_customer_df(orders_df,customers_df)
 
 st.header("E-Commerce Dashboard")
 
